faeb05bd171d6f795b27411688ea0f7e629a0eb1
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-07-11 23:55:30 +0200

    add canvas

--- weather/app.js
+++ weather/app.js
@@ -3,11 +3,43 @@
 window.addEventListener('load', () => {
   let long;
   let lat;
+  let tempretureDeegree = document.querySelector('.temperauter-degree');
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let locationTimeZone = document.querySelector('.location-timezone');
+
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-     
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/13f54ac665fa61b6cf809d0cb273aa8a/${lat},${long}`;
+
+      fetch(api)
+        .then(Response => {
+          return Response.json();
+        })
+        .then(data => {
+          const { temperature, summary, icon } = data.currently;
+          // set DOM elements from  the API
+
+          tempretureDeegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimeZone.textContent = data.timezone;
+
+          // set icon 
+          setIcons(icon, document.querySelector(".icon"));
+        });
     });
   }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, skycons[currentIcon]);
+  }
+
+
 });
