f683ee57babd89c42db7058163c4b8dca6038b73
jafarimahdi <mahdijafarii.mj@gmail.com>
2020-06-06 17:06:24 +0200

    fliping the cards

--- javaScript-projects/memory_card_game/main.js
+++ javaScript-projects/memory_card_game/main.js
@@ -1,6 +1,33 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
 
-function flipCard(){
-    this.classList.toggle('flip');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.toggle("flip");
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+
+        // do cards match?
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            //  it's match
+            firstCard.removeEventListener("click", flipCard);
+            secondCard.removeEventListener("click", flipCard);
+        } else {
+            // not a match  
+            setTimeout(() => {
+            firstCard.classList.remove("flip");
+            secondCard.classList.remove("flip");
+        },1500);
+        }
+    }
 }
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach((card) => card.addEventListener("click", flipCard));
